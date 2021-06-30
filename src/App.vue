e5718b263db8083067bb8613319df5c54d9628a2
James <dev.jpsmith@gmail.com>
2021-06-29 20:19:50 -0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,90 @@
+<template>
+  <h1>{{ title }}</h1>
+  <div v-if="showModal">
+    <Modal theme="sale" @close="() => showModal = false">
+      <h1>Super Giveaway</h1>
+      <p>Grab your stuff NOW!</p>
+      <template v-slot:links>
+        <a href="#">Gift 1</a>
+        <a href="#">Gift 2</a>
+        <a href="#">Gift 3</a>
+      </template>
+    </Modal>
+  </div>
+  <div v-if="showMyModal">
+    <Modal id="myModal" @close="() => showMyModal = false">
+      <h1>My Modal</h1>
+      <p>This is a plain modal</p>
+    </Modal>
+  </div>
+  <button id="openModalBtn" @click="toggleModal($event)">Open Modal</button>
+  <br>
+  <button id="openMyModalBtn" @click="toggleModal($event)">Open My Modal</button>
+</template>
+
+<script>
+import Modal from './components/Modal';
+
+export default {
+  name: 'App',
+  components: {
+    Modal,
+  },
+  data() {
+    return {
+      title: 'Modal App',
+      showModal: false,
+      showMyModal: false
+    }
+  },
+  methods: {
+    handleClick() {
+      console.log(this.$refs.name)
+      this.$refs.name.classList.add('active');
+      this.$refs.name.focus();
+    },
+    toggleModal(e) {
+      if (e && e.srcElement?.id === 'openMyModalBtn') {
+        console.log(e);
+        this.showMyModal = !this.showMyModal;
+      } else {
+        this.showModal = !this.showModal;
+      }
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  align-items: center;
+}
+
+h1 {
+  border-bottom: 1px solid #ddd;
+  padding-bottom: 10px;
+}
+
+.active {
+  border: 5px solid #2d8149;
+}
+
+button {
+  border: none;
+  min-width: 200px;
+  font-weight: bold;
+  text-transform: uppercase;
+  height: 30px;
+  margin-bottom: 10px;
+  border-radius: 4px;
+  color: #ddd;
+  background-color: #333;
+}
+
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="backdrop" @click.self="closeModal">
+        <div class="modal" :class="{ sale: theme === 'sale' }">
+            <slot></slot>
+            <div class="actions">
+                <slot name="links" />
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        theme: {
+            type: String,
+            default: ''
+        }
+    },
+    methods: {
+        closeModal() {
+            this.$emit('close', this);
+        }
+    },
+}
+</script>
+
+<style>
+    .modal {
+        width: 400px;
+        padding: 20px;
+        margin: 100px auto;
+        background: #fff;
+        border-radius: 10px;
+    }
+
+    .backdrop {
+        top: 0;
+        position: fixed;
+        background: rgb(0, 0, 0, 0.5);
+        width: 100%;
+        height: 100%;
+    }
+
+    .modal.sale {
+        background: crimson;
+        color: white;
+    }
+
+    .modal.sale.h1 {
+        color: white;
+    }
+
+    .modal .actions a {
+        color: #333;
+        padding: 8px;
+        border: 1px solid #eee;
+        border-radius: 4px;
+        text-decoration: none;
+        margin: 10px;
+    }
+
+    .modal.sale .actions a {
+        color: #eee;
+    }   
+</style>
